298b47195914d3dbb46c7f97ed69825fbf09e0ef
bateso88 <bateso@hotmail.co.uk>
2020-12-15 12:31:53 +0000

    input_students method added

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 # Student array
+=begin
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -12,9 +13,29 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # creat empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to array
+    students << {name: name, cohort: :november}
+    # get another name from user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 # and then print
 def print_header
-  puts "The student of Villains Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
@@ -28,6 +49,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
